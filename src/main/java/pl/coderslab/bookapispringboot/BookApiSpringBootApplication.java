460c2bae8fc88389e1aa6f72c7add5f27c27d921
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-09-11 23:02:50 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/bookapispringboot/BookApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.bookapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/coderslab/bookapispringboot/models/Book.java
@@ -0,0 +1,25 @@
+package pl.coderslab.bookapispringboot.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+}
